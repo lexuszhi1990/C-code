6d211c90b7fe4eb8462e765e5a12c58ffed5d5da
david <lexuszhi1990@gmail.com>
2011-02-19 23:07:06 +0800

    josephus finished

--- josephus_task_15/stack_josephus.c
+++ josephus_task_15/stack_josephus.c
@@ -1,36 +1,68 @@
 #include <stdio.h>
-int total = 10;
+#include <stdlib.h>
+int total = 100;
 int counter = 3;
+typedef struct person *per;
 struct person {
     int num;
-    struct preson *next;
+    per next;
 };
-typedef struct person *per;
 
-
-
-pre init_link(int n)
+per init_link(int n, per p)
 {
-    per p = NULL;
-    if((p = malloc(sizeof(struct person))) == NULL)
+	int i;
+    for (i = 1; i < n; i++) 
+    {
+    if((p->next = malloc(sizeof(struct person))) == NULL)
     {
         perror("ess");
         exit(0);
     }
-    p->num = 0;
-    p->next = NULL;
+    p->next->num = i;
+    p->next->next = NULL;
     p = p->next;
-    for (i = 1; i < n; i++) 
-    {
-        
     }
+	return p;	     
+}
+
+void print_link(per p)
+{
+	per stop = p;
+	printf("%-6d",p->num);
+	p = p->next;
+	while (p != stop) 
+	{
+		printf("%-6d",p->num);
+		p = p->next;
+	}
+	putchar('\n');
+}
+
+void del_link(per p)
+{
+	per *pp = &p;
+	int i = 1;
+	while (*pp != (*pp)->next) 
+	{
+		if(i == 3)
+		{
+			i = 1;
+			printf("%-6d ", (*pp)->num + 1);
+			*pp = (*pp)->next;
+		}
+		pp = &(*pp)->next;				
+		i++;
+	}
+	printf("\n%d survived\n", (*pp)->num + 1);
 }
 
 int main(int argc, const char *argv[])
 {
-    per head = NULL;
-    head = init_link(total);
-    print_link(head);
-    head = del_link();
+	per head = malloc(sizeof(struct person));
+	head->num = 0;
+	per ss=  init_link(total, head);
+	ss->next = head;
+        print_link(head);
+        del_link(head);
     return 0;
 }
